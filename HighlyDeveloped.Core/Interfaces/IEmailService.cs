e6180905516d2405fa8664c5ee334e404e1a13b5
Moueidat123 <mohamad.oueidat@ogilvy.com>
2025-05-21 14:44:33 +0300

    Email Service

---
+++ HighlyDeveloped.Core/Interfaces/IEmailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HighlyDeveloped.Core.ViewModel;
+
+namespace HighlyDeveloped.Core.Interfaces
+{
+    /// <summary>
+    /// Handles all emaill operations
+    /// </summary>
+    public interface IEmailService
+    {
+        void SendContactNotificationToAdmin(ContactFormViewModel vm);
+    }
+}
---
+++ HighlyDeveloped.Core/RegisterServicesComposer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HighlyDeveloped.Core.Interfaces;
+using HighlyDeveloped.Core.Services;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace HighlyDeveloped.Core
+{
+    public class RegisterServicesComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Register<IEmailService, EmailService>(Lifetime.Request);
+        }
+    }
+}
