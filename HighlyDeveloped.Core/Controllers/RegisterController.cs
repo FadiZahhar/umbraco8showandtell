4028f7c0ee07c76614e487741c7496d4f62ee049
sereneogilvy <137052801+sereneogilvy@users.noreply.github.com>
2025-05-22 11:40:22 +0300

    section 17 - 86

--- HighlyDeveloped.Core/Controllers/RegisterController.cs
+++ HighlyDeveloped.Core/Controllers/RegisterController.cs
@@ -17,21 +17,94 @@ namespace HighlyDeveloped.Core.Controllers
     /// <summary>
     /// Handle member registration
     /// </summary>
-    /// 
-    //inherits from surface controller
     public class RegisterController : SurfaceController
     {
         private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/";
+        private IEmailService _emailService;
 
-     
+        public RegisterController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
 
-
+       
+        /// <summary>
+        /// Render the registration form
+        /// </summary>
+        /// <returns></returns>
         public ActionResult RenderRegister()
         {
             var vm = new RegisterViewModel();
             return PartialView(PARTIAL_VIEW_FOLDER + "Register.cshtml", vm);
         }
 
-    
+
+        //hANDLE REGISTERING THE CONTROLLER POST
+
+        /// <summary>
+        /// Handle the registration form post
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns></returns>
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult HandleRegister(RegisterViewModel vm)
+        {
+            //If form not valid - return
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            //Check if there is already a member with that email address
+            var existingMember = Services.MemberService.GetByEmail(vm.EmailAddress);
+
+            if (existingMember != null)
+            {
+                ModelState.AddModelError("Account Error", "There's already a user with that email address.");
+                return CurrentUmbracoPage();
+            }
+
+            //Check if their username is already in use
+            existingMember = Services.MemberService.GetByUsername(vm.Username);
+
+            if (existingMember != null)
+            {
+                ModelState.AddModelError("Account Error", "There's already a user with that username. Please choose a different one.");
+                return CurrentUmbracoPage();
+            }
+
+            //Create "member" in Umbraco with the details
+            var newMember = Services.MemberService
+                                .CreateMember(vm.Username, vm.EmailAddress, $"{vm.FirstName} {vm.LastName}", "Member");
+            newMember.PasswordQuestion = "";
+            newMember.RawPasswordAnswerValue = "";
+            //Need to save the member before you can set the password
+            Services.MemberService.Save(newMember);
+            Services.MemberService.SavePassword(newMember, vm.Password);
+            //Assign a role - i.e. Normal User
+            Services.MemberService.AssignRole(newMember.Id, "Normal User");
+
+            return CurrentUmbracoPage();
+
+            ////Create email verification token
+            ////Token creation
+            //var token = Guid.NewGuid().ToString();
+            //newMember.SetValue("emailVerifyToken", token);
+            //Services.MemberService.Save(newMember);
+
+            ////Send email verification
+            //_emailService.SendVerifyEmailAddressNotification(newMember.Email, token);
+
+
+            ////Thank the user
+            ////Return confirmation message to user
+            //TempData["status"] = "OK";
+
+            //return RedirectToCurrentUmbracoPage();
+        }
+        
+
+       
     }
 }
