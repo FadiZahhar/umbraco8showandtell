b2d51f3d31484829a3d988849bb8ddae3c7be21e
sereneogilvy <137052801+sereneogilvy@users.noreply.github.com>
2025-05-22 14:18:37 +0300

    section 18 - 92

---
+++ HighlyDeveloped.Core/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using HighlyDeveloped.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace HighlyDeveloped.Core.Controllers
+{
+    public class LoginController : SurfaceController
+
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Login/";
+
+
+        public ActionResult RenderLogin()
+        {
+            var vm = new LoginViewModel();
+            vm.RedirectUrl = HttpContext.Request.Url.AbsolutePath;
+            return PartialView(PARTIAL_VIEW_FOLDER + "Login.cshtml", vm);
+        }
+
+
+    }
+}
