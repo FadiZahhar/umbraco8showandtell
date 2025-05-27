f0dc3c55e1a7ab5548b21848c1b380066f765813
sereneogilvy <137052801+sereneogilvy@users.noreply.github.com>
2025-05-27 12:49:09 +0300

    updates for Npoco Tables

---
+++ HighlyDeveloped.Web/App_Code/Recipes/Models/pocos/Recipe.cs
@@ -0,0 +1,54 @@
+﻿using NPoco;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace HighlyDeveloped.Models.pocos
+{
+
+    [TableName("CMSRecipes")]
+    [PrimaryKey("RecipeId", AutoIncrement = true)]
+    [ExplicitColumns]
+
+    public class recipe
+    {
+
+        [Column("RecipeId")]
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int RecipeId { get; set; }
+
+        [Column("RecipeName")]
+        [Length(250)]
+        public string RecipeName { get; set; }
+
+        [Column("Description")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string Description { get; set; }
+
+        [Column("Directions")]
+        [SpecialDbType(SpecialDbTypes.NTEXT)]
+        public string Directions { get; set; }
+
+        [Column("ServingSize")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        [Length(100)]
+        public string ServingSize { get; set; }
+
+        [Column("DateSubmitted")]
+        public DateTime DateSubmitted { get; set; }
+
+        [Column("DateApproved")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public DateTime DateApproved { get; set; }
+
+        [Column("UserId")]
+        [NullSetting(NullSetting = NullSettings.Null)]
+        public int UserId { get; set; }
+
+        [Column("CategoryID")]
+        public int CategoryID { get; set; }
+
+    }
+}
---
+++ HighlyDeveloped.Web/App_Code/Recipes/PetaPocoApplicationEventHandler.cs
@@ -0,0 +1,23 @@
+﻿//using Recipes.Models.pocos;
+
+//using Spring.Context;
+//using Umbraco.Core;
+//using Umbraco.Core.Persistence;
+//using Umbraco.Web;
+
+//namespace Recipes
+//{
+//    public class PetaPocoApplicationEventHandler : IUserComposer
+//    {
+//        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+//        {
+//            var ctx = applicationContext.DatabaseContext;
+//            var db = new DatabaseSchemaHelper(ctx.Database, applicationContext.ProfilingLogger.Logger, ctx.SqlSyntax);
+
+//            if (!db.TableExist("CMSRecipes"))
+//            {
+//                db.CreateTable<recipe>(false);
+//            }
+//        }
+//    }
+//}
