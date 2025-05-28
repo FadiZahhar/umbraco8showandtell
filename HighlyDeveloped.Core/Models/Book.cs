c55887fcd8c155d8c02a4e43bb8a9273eccb3485
Moueidat123 <mohamad.oueidat@ogilvy.com>
2025-05-28 10:52:33 +0300

    update

---
+++ HighlyDeveloped.Core/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using NPoco;
+
+namespace HighlyDeveloped.Core.Models
+{
+    [TableName("Books")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string ISBN { get; set; }
+    }
+}
