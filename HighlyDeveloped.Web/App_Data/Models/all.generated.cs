516efca828f87556aad0c4acd176661db5f6ce38
Moueidat123 <mohamad.oueidat@ogilvy.com>
2025-08-05 10:59:55 +0300

    add class

--- HighlyDeveloped.Web/App_Data/Models/all.generated.cs
+++ HighlyDeveloped.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "61196fb971694747")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
