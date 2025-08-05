ff53956ede61131ce72a85e6d23500655a496af0
Moueidat123 <mohamad.oueidat@ogilvy.com>
2025-08-05 10:06:01 +0300

    update

--- HighlyDeveloped.Web/App_Data/Models/models.generated.cs
+++ HighlyDeveloped.Web/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder.Embedded;
 
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "61196fb971694747")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
 
 namespace Umbraco.Web.PublishedModels
 {
