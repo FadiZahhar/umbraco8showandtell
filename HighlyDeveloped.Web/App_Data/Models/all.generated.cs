b9196fdfef6c4ee1d8cf3fd7939926a0c9bb7311
sereneogilvy <137052801+sereneogilvy@users.noreply.github.com>
2025-08-13 14:54:59 +0300

    add popular items

--- HighlyDeveloped.Web/App_Data/Models/all.generated.cs
+++ HighlyDeveloped.Web/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "78449714c6943dfc")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
